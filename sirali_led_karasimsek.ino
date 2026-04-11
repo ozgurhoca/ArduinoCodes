b3d58dd98ddf206a64424ccb4040fef85e19c448
ozgurhoca <46501512+ozgurhoca@users.noreply.github.com>
2026-04-11 23:37:11 +0300

    Refactor LED control to use multiple outputs night rider
    Updated the LED control logic to include multiple LEDs with a defined delay.

--- sirali_led_karasimsek.ino
+++ sirali_led_karasimsek.ino
@@ -1,24 +1,49 @@
-int k=8;
-int s=9;
-int y=10;
+int l1=2;
+int l2=3;
+int l3=4;
+int l4=5;
+int l5=6;
+int l6=7;
+int sure=100;
 
 void setup() {
-  pinMode(k,OUTPUT);
-  pinMode(s,OUTPUT);
-  pinMode(y,OUTPUT);
+  pinMode(l1,OUTPUT);
+  pinMode(l2,OUTPUT);
+  pinMode(l3,OUTPUT);
+  pinMode(l4,OUTPUT);
+  pinMode(l5,OUTPUT);
+  pinMode(l6,OUTPUT);
 }
 
 void loop() {
-  digitalWrite(k,HIGH);
-  delay(2000);
-  digitalWrite(k,LOW);
-  digitalWrite(s,HIGH);
-  delay(200);
-  digitalWrite(s,LOW);
-  digitalWrite(y,HIGH);
-  delay(2000);
-  digitalWrite(y,LOW);
-  digitalWrite(s,HIGH);
-  delay(200);
-  digitalWrite(s,LOW);
+  digitalWrite(l1,HIGH);
+  delay(sure);
+  digitalWrite(l1,LOW);
+  digitalWrite(l2,HIGH);
+  delay(sure);
+  digitalWrite(l2,LOW);
+  digitalWrite(l3,HIGH);
+  delay(sure);
+  digitalWrite(l3,LOW);
+  digitalWrite(l4,HIGH);
+  delay(sure);
+  digitalWrite(l4,LOW);
+  digitalWrite(l5,HIGH);
+  delay(sure);
+  digitalWrite(l5,LOW);
+  digitalWrite(l6,HIGH);
+  delay(sure);
+  digitalWrite(l6,LOW);
+  digitalWrite(l5,HIGH);
+  delay(sure);
+  digitalWrite(l5,LOW);
+  digitalWrite(l4,HIGH);
+  delay(sure);
+  digitalWrite(l4,LOW);
+  digitalWrite(l3,HIGH);
+  delay(sure);
+  digitalWrite(l3,LOW);
+  digitalWrite(l2,HIGH);
+  delay(sure);
+  digitalWrite(l2,LOW);
 }
