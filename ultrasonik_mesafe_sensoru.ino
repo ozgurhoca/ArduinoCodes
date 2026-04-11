e22d29be18f12f58f20a2eec5d9d282db3d03ddf
ozgurhoca <46501512+ozgurhoca@users.noreply.github.com>
2026-04-11 23:37:52 +0300

    use ultrasonic sensor for distance measurement

--- ultrasonik_mesafe_sensoru.ino
+++ ultrasonik_mesafe_sensoru.ino
@@ -1,49 +1,17 @@
-int l1=2;
-int l2=3;
-int l3=4;
-int l4=5;
-int l5=6;
-int l6=7;
-int sure=100;
+#define trig 8
+#define echo 9
 
 void setup() {
-  pinMode(l1,OUTPUT);
-  pinMode(l2,OUTPUT);
-  pinMode(l3,OUTPUT);
-  pinMode(l4,OUTPUT);
-  pinMode(l5,OUTPUT);
-  pinMode(l6,OUTPUT);
+  Serial.begin(9600);
+  pinMode(trig,OUTPUT);
+  pinMode(echo,INPUT);
 }
-
 void loop() {
-  digitalWrite(l1,HIGH);
-  delay(sure);
-  digitalWrite(l1,LOW);
-  digitalWrite(l2,HIGH);
-  delay(sure);
-  digitalWrite(l2,LOW);
-  digitalWrite(l3,HIGH);
-  delay(sure);
-  digitalWrite(l3,LOW);
-  digitalWrite(l4,HIGH);
-  delay(sure);
-  digitalWrite(l4,LOW);
-  digitalWrite(l5,HIGH);
-  delay(sure);
-  digitalWrite(l5,LOW);
-  digitalWrite(l6,HIGH);
-  delay(sure);
-  digitalWrite(l6,LOW);
-  digitalWrite(l5,HIGH);
-  delay(sure);
-  digitalWrite(l5,LOW);
-  digitalWrite(l4,HIGH);
-  delay(sure);
-  digitalWrite(l4,LOW);
-  digitalWrite(l3,HIGH);
-  delay(sure);
-  digitalWrite(l3,LOW);
-  digitalWrite(l2,HIGH);
-  delay(sure);
-  digitalWrite(l2,LOW);
+  digitalWrite(trig,HIGH);
+  delayMicroseconds(15);
+  digitalWrite(trig,LOW);
+  int sure=pulseIn(echo,HIGH);
+  int mesafe=sure*0.0343/2;
+  Serial.println(mesafe);
+  delay(50);
 }
