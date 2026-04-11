906b4e9ca67a1170c7793b8ea870560118105f3c
ozgurhoca <46501512+ozgurhoca@users.noreply.github.com>
2026-04-11 23:32:56 +0300

    Replace potentiometer control with tone generation

--- pasif_buzzer.ino
+++ pasif_buzzer.ino
@@ -1,19 +1,13 @@
 int buz=6;
-int pot=A0;
-int potOkunan;
 
 void setup() {
   pinMode(buz,OUTPUT);
-  Serial.begin(9600);
 }
-
 void loop() {
-  potOkunan=analogRead(A0); //okunan değer 0-1023 arasında
-  int sesSiddeti=map(potOkunan,0,1023,0,255);
-  analogWrite(buz,sesSiddeti); //yazılan değer 0-255 arasında
-  Serial.print("potansiyometre:");
-  Serial.print(potOkunan);
-  Serial.print("   buzzer:");
-  Serial.println(sesSiddeti);
-  delay(50);
+  tone(buz,500); //20-20000
+  delay(500);
+  tone(buz,1500);
+  delay(500);
+  tone(buz,2500);
+  delay(500);
 }
