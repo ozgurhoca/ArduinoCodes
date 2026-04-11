ed0bfde900c9979feb6506a9cbd45e8d7a83c38c
ozgurhoca <46501512+ozgurhoca@users.noreply.github.com>
2026-04-11 23:39:25 +0300

    buzzer in aktif
    Replaced LED control with buzzer activation in loop.

--- aktif_buzzer.ino
+++ aktif_buzzer.ino
@@ -1,17 +1,11 @@
-#define led 8
-int okunan;
+int buz=6;
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(led,OUTPUT);
+  pinMode(buz,OUTPUT);
 }
 void loop() {
-  okunan=analogRead(A0);
-  Serial.println(okunan);
-  if (okunan<20) {  // 20 değeri ldr'den gelen analog sinyale göre belirlenmiştir
-    digitalWrite(led,HIGH);
-  }
-  else {
-    digitalWrite(led,LOW);
-  }
+  digitalWrite(buz,HIGH);
+  delay(500);
+  digitalWrite(buz,LOW);
+  delay(500);
 }
